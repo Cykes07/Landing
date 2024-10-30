c04774135dfc6c7a770eb5f3ca489610bb721383
emjorome <emjorome@espol.edu.ec>
2024-10-30 14:21:58 -0500

    Cykes07 Formulario
    Hola mundo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,105 @@
+const databaseURL = 'https://landing-1ec68-default-rtdb.firebaseio.com/coleccion.json'; 
+
+let sendData = ( ) => {  
+
+    
+    // Obtén los datos del formulario
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+        form.reset()
+        
+    })
+    .catch(error => {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    }); 
+
+}
+
+let getData = async () => {  
+
+    try {
+
+        // Realiza la petición fetch a la URL de la base de datos
+        const response = await fetch(databaseURL, {
+            method: 'GET'
+        });
+
+        // Verifica si la respuesta es exitosa
+        if (!response.ok) {
+          alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        }
+
+        // Convierte la respuesta en formato JSON
+        const data = await response.json();
+
+        if(data != null) {
+
+            // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+
+            // END
+            let countSuscribers = new Map()
+
+            if (Object.keys(data).length > 0) {
+                for (let key in data) {
+       
+                    let { email, saved } = data[key]
+                       
+                    let date = saved.split(",")[0]
+                       
+                    let count = countSuscribers.get(date) || 0;
+                    countSuscribers.set(date, count + 1)
+                }
+            }
+            
+
+            // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+            if (countSuscribers.size > 0) {
+
+                subscribers.innerHTML = ''
+       
+                let index = 1;
+                for (let [date, count] of countSuscribers) {
+                    let rowTemplate = `
+                    <tr>
+                        <th>${index}</th>
+                        <td>${date}</td>
+                        <td>${count}</td>
+                    </tr>`
+                    subscribers.innerHTML += rowTemplate
+                    index++;
+                }
+            }
+            // END
+
+        }
+
+      } catch (error) {
+        // Muestra cualquier error que ocurra durante la petición
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    }
+
+}
+
+
 let ready = () => {
     console.log('DOM está listo')
+    getData()
     //debugger;
 }
 let loaded = () => {
@@ -36,34 +136,3 @@ let loaded = () => {
 
 window.addEventListener("DOMContentLoaded", ready);
 window.addEventListener("load", loaded)
-
-const databaseURL = 'https://landing-1ec68-default-rtdb.firebaseio.com/coleccion.json'; 
-
-let sendData = ( ) => {  
-
-    // Obtén los datos del formulario
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
-    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
-    fetch(databaseURL, {
-        method: 'POST', // Método de la solicitud
-        headers: {
-            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
-        },
-        body: JSON.stringify(data) // Convierte los datos a JSON
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.statusText}`);
-        }
-        return response.json(); // Procesa la respuesta como JSON
-    })
-    .then(result => {
-        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
-        form.reset()
-    })
-    .catch(error => {
-        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
-    }); 
-
-}
